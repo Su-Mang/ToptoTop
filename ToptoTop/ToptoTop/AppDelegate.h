399beb8c247b179ca4b9722cb9397f9e084ba4ce
yjx <yuejingxiang@xiyou3g.com>
2020-04-21 13:21:59 +0800

    项目创建

---
+++ ToptoTop/ToptoTop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToptoTop
+//
+//  Created by 岳靖翔 on 2020/4/21.
+//  Copyright © 2020 岳靖翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToptoTop/ToptoTop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToptoTop
+//
+//  Created by 岳靖翔 on 2020/4/21.
+//  Copyright © 2020 岳靖翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
